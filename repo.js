2a70d5f61ca33678802f260563c0ecd0135935f1
felix <you@example.com>
2025-05-03 12:10:25 -0500

    a

--- repo.js
+++ repo.js
@@ -3,8 +3,9 @@
  * This ensures we're using a single consistent client with the same configuration.
  */
 
-import RepoMD from "./repo-old-local-copy/index.js";
-
+//import RepoMD from "./repo-old-local-copy/index.js";
+// import RepoMD from "../repo-client/src/lib/index.js";
+import RepoMD from "repo-md";
 // Create and export a singleton instance of the RepoMD client
 const repo = new RepoMD({
   orgSlug: "iplanwebsites",
@@ -13,12 +14,12 @@ const repo = new RepoMD({
 
   projectId: "680e97604a0559a192640d2c",
 
-  // rev: "68135ef83eb888fca85d2645", //SHOULD default to latest if none are passed.
+  // rev: "68135ef83eb888fca85d2645", //  default to latest release if none are passed.
 
   debug: true,
 
-  project: "680e97604a0559a192640d2c", //will be retired
-  org: "iplanwebsites",
+  //project: "680e97604a0559a192640d2c", //will be retired
+  //org: "iplanwebsites",
 });
 
 export default repo;
